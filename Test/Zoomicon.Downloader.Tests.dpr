476bc2119c5822b7b0ba81bc19f201b63c6a74b9
birbilis <birbilis@zoomicon.com>
2024-10-21 22:06:33 +0300

    Moved to separate repository from READCOM_App project. May need fixing of paths for Test project.

---
+++ Test/Zoomicon.Downloader.Tests.dpr
@@ -0,0 +1,29 @@
+program Zoomicon.Downloader.Tests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  Test.Zoomicon.Downloader in 'Test.Zoomicon.Downloader.pas',
+  Zoomicon.Downloader.Classes in '..\Zoomicon.Downloader.Classes.pas',
+  Zoomicon.Downloader.Models in '..\Zoomicon.Downloader.Models.pas';
+
+{$R *.RES}
+
+begin
+  ReportMemoryLeaksOnShutdown := True; //added to check memory leaks on exit
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
